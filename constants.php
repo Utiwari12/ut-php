b08dd0289608503659402f2706cc617ee138db70
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-02-23 00:05:17 +0530

    constants

---
+++ constants.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constants in PHP</title>
+</head>
+<body>
+   <?php
+   //constants defined using the define()
+   define("PI",3.14);
+   echo PI;
+   echo "<br>";
+   
+   
+   ?>
+</body>
+</html>

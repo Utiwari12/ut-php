f13e40b94003d1ba037af96ea523680a05df04d4
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-02-23 00:04:44 +0530

    data types

---
+++ datatype.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Type</title>
+</head>
+<body>
+    <?php
+    class Test{
+        
+    }
+    // $a = 10; // integer
+    // echo $a. "<br>";
+    // $b = 10.5; // float
+    // echo $b. "<br>";
+    // $c = "Hello"; // string
+    // echo $c. "<br>";
+    // $d = true; // boolean
+    // echo $d. "<br>";
+    // $arr = [1,2,3,4,5]; // array
+    // print_r($arr). "<br>";
+    // $arr2 = array(1,2,3,4,5); // array
+    // print_r($arr2). "<br>";
+   // $obj = new stdClass(); // object
+    
+    //$e = null; // null
+    //echo $e. "<br>";
+    //$t = new Test(); // object
+    
+
+    ?>
+</body>
+</html>

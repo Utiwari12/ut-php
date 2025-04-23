aa950cd1fc7be49f86e6787e9da53b2312aa010e
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-04-24 00:18:25 +0530

    die exist

---
+++ die_exit.php
@@ -0,0 +1,26 @@
+<?php
+$a = 20;
+$b = 30;
+echo $a+$b;
+echo "<br>";
+echo $a-$b;
+echo "<br>";
+echo $a*$b;
+echo "<br>";
+//return;
+echo $a/$b;
+
+echo "<br>";
+//die("Bye");
+echo $a%$b;
+echo "<br>";
+//die();
+echo $a**$b;
+echo "<br>";
+//exit();
+echo $a++;
+echo "<br>";
+echo $a--;
+echo "<br>";
+
+?>

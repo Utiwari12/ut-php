737aaf2074437150f86a7244c47102ef19d9ca60
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-04-24 13:46:32 +0530

    config

---
+++ mySQL/config.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$userName = "root";
+$password = "";
+//$database = "dbsmschool";
+
+$conn = new PDO("mysql:host=$host;dbname=dbsmschool", $userName, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//echo "Connected Successfully";
+?>

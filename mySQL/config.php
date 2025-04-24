af9953e880dc4840d2873fa811e797501406d795
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-04-24 14:27:19 +0530

    Configuration

--- mySQL/config.php
+++ mySQL/config.php
@@ -6,5 +6,5 @@ $password = "";
 
 $conn = new PDO("mysql:host=$host;dbname=dbsmschool", $userName, $password);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//echo "Connected Successfully";
+echo "Connected Successfully";
 ?>

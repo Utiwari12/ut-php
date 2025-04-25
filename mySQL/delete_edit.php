6b3c6bbf0e3f0d739b51ae50e759b32618f4278c
Utiwari12 <motioniitjeeinstitute@gmail.com>
2025-04-25 17:48:28 +0530

    delete edit function

---
+++ mySQL/delete_edit.php
@@ -0,0 +1,43 @@
+<?php
+include 'config.php';
+//$students = $conn->prepare("DELETE  FROM `student` where id = '1'");
+//echo $students->execute();
+$students = $conn->prepare("SELECT * FROM `student`");
+$students->execute();
+$result = $students->fetchAll();
+//print_r($result);
+
+echo "<table border = '1'>";
+     foreach ($result as $student) {
+      echo "<tr> 
+             <td>".$student['id']. "</td>
+             <td>".$student['stname']. "</td>
+             <td>".$student['course']. "</td>
+             <td>".$student['fees']. "</td>
+
+             <td>
+              <form method='post'>
+              <button name='delete' value='".$student['id']."'>Delete</button>
+
+              </form>
+             </td>
+
+
+      </tr>";
+     
+    
+     }
+echo "</table>";
+
+if (isset($_POST['delete'])) {
+  $id = $_POST['delete'];
+  $students = $conn->prepare("DELETE FROM `student` where id = '$id'");
+  if ($students->execute()) {
+    echo "Record Deleted Successfully";
+    header("Refresh:0");  // here 0 means refresh current page in 0 seconds
+    exit();
+  }else {
+    echo "Not Deleted";
+  }
+}
+?>
